6768814ff5b3a729e631e347372b921c58ef07ca
mohammad <mshayganfar@wpi.edu>
2016-03-28 11:48:10 -0400

    Added toggle service, client and server.

---
+++ build/agitr/CMakeFiles/Spawn_turtle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Spawn_turtle.dir/Spawn_turtle.cpp.o"
+  "/home/mohammad/catkin_ws/devel/lib/agitr/Spawn_turtle.pdb"
+  "/home/mohammad/catkin_ws/devel/lib/agitr/Spawn_turtle"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Spawn_turtle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/agitr/CMakeFiles/pubvel_toggle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pubvel_toggle.dir/pubvel_toggle.cpp.o"
+  "/home/mohammad/catkin_ws/devel/lib/agitr/pubvel_toggle.pdb"
+  "/home/mohammad/catkin_ws/devel/lib/agitr/pubvel_toggle"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pubvel_toggle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
